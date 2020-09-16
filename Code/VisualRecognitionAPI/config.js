e5f2f967dda8c72b9743bc9c63cabf37351352cd
Ritu Maheshwari <36675485+ritugm@users.noreply.github.com>
2020-09-16 16:01:42 +0530

    Deleted file

--- Code/VisualRecognitionAPI/config.js
+++
@@ -1,8 +0,0 @@
-var config =
-        {
-        apikey: "_FX4CKbE3rrqdtzGC5BfRholsnaubV1mQla0he69FKx5",
-        username: "rajsuratwala06@gmail.com",
-        password: "Master80"
-        };
-
-module.exports = config;
