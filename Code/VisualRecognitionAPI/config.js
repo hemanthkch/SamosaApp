5f2d96619b39161ef2f966617aa9913ee0700b46
Ritu Maheshwari <36675485+ritugm@users.noreply.github.com>
2020-09-16 15:58:10 +0530

    Added config file

---
+++ Code/VisualRecognitionAPI/config.js
@@ -0,0 +1,8 @@
+var config =
+        {
+        apikey: "_FX4CKbE3rrqdtzGC5BfRholsnaubV1mQla0he69FKx5",
+        username: "rajsuratwala06@gmail.com",
+        password: "Master80"
+        };
+
+module.exports = config;
